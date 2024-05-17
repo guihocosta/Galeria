d22c31cb47cc05b624b667c5ea1c36e6311230d9
Guilherme Costa <guiihocosta@gmail.com>
2024-05-17 08:55:54 -0300

    Primeira Dia fazendo Galaria

--- app/src/main/java/omena/guilherme/galeria/PhotoActivity.java
+++ app/src/main/java/omena/guilherme/galeria/PhotoActivity.java
@@ -4,21 +4,24 @@ import android.os.Bundle;
 import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class PhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_photo);
 
-        Toolbar toolbar = findViewById(R.id.tbMain);
+        Toolbar toolbar = findViewById(R.id.tbPhoto);
         setSupportActionBar(toolbar);
-
+        
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
     private void setSupportActionBar(Toolbar toolbar) {
